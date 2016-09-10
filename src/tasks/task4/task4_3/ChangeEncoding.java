b17e49b0e295f301100aff1ea7f96dafd396bb72
Egor Kozyrev <towel98@yandex.ru>
2016-09-10 03:05:08 +0300

    task4_3

---
+++ src/tasks/task4/task4_3/ChangeEncoding.java
@@ -0,0 +1,47 @@
+package tasks.task4.task4_3;
+
+import java.io.*;
+
+/**
+ * Created by Egor on 10.09.2016.
+ */
+public class ChangeEncoding {
+
+    private static final String INPUT_FILE_NAME = "C:/Users/Egor/IdeaProjects/Homework/src/tasks/task4/task4_3/utf-8-file.txt";
+    private static final String OUTPUT_FILE_NAME = "C:/Users/Egor/IdeaProjects/Homework/src/tasks/task4/task4_3/utf-16-file.txt";
+    private static final int READ_FILE_BUFFER_SIZE = 100;
+
+
+    public static boolean rewriteFileUTF8ToUTF16() {
+        String text = ChangeEncoding.readFileUTF8();
+        return ChangeEncoding.printTextIntoFileUTF16(text);
+    }
+
+    public static String readFileUTF8() {
+        StringBuilder text = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(INPUT_FILE_NAME), "UTF-8"))) {
+            int symbolsRead;
+            char[] buffer = new char[READ_FILE_BUFFER_SIZE];
+
+            while ((symbolsRead = bufferedReader.read(buffer)) > 0) {
+                text.append(buffer, 0, symbolsRead);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text.toString();
+    }
+
+    public static boolean printTextIntoFileUTF16(String text) {
+
+        try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(OUTPUT_FILE_NAME), "UTF-16")) {
+            outputStreamWriter.write(text);
+            outputStreamWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/task4/task4_3/ChangeEncodingTest.java
@@ -0,0 +1,18 @@
+package test.task4.task4_3;
+
+import org.junit.Test;
+import tasks.task4.task4_3.ChangeEncoding;
+
+/**
+ * Created by Egor on 10.09.2016.
+ */
+public class ChangeEncodingTest {
+
+    @Test
+    public void rewriteFileTest() {
+        ChangeEncoding.rewriteFileUTF8ToUTF16();
+    }
+
+}
+
+
