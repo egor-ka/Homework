9c589a70e0a06d117c3aaacec44ae7d84039a771
Egor Kozyrev <towel98@yandex.ru>
2016-09-19 15:20:01 +0300

    task4_3 merged 3 methods in one

--- src/tasks/task4/task4_3/ChangeEncoding.java
+++ src/tasks/task4/task4_3/ChangeEncoding.java
@@ -9,39 +9,22 @@ public class ChangeEncoding {
 
     private static final String INPUT_FILE_NAME = "C:/Users/Egor/IdeaProjects/Homework/src/resources/task4/task4_3/utf-8-file.txt";
     private static final String OUTPUT_FILE_NAME = "C:/Users/Egor/IdeaProjects/Homework/src/resources/task4/task4_3/utf-16-file.txt";
-    private static final int READ_FILE_BUFFER_SIZE = 100;
-
+    private static final int READ_FILE_BUFFER_SIZE = 10;
 
     public static boolean rewriteFileUTF8ToUTF16() {
-        String text = ChangeEncoding.readFileUTF8();
-        return ChangeEncoding.printTextIntoFileUTF16(text);
-    }
-
-    public static String readFileUTF8() {
-        StringBuilder text = new StringBuilder();
-
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(INPUT_FILE_NAME), "UTF-8"))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(INPUT_FILE_NAME), "UTF-8"));
+             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(OUTPUT_FILE_NAME), "UTF-16"))) {
             int symbolsRead;
             char[] buffer = new char[READ_FILE_BUFFER_SIZE];
 
             while ((symbolsRead = bufferedReader.read(buffer)) > 0) {
-                text.append(buffer, 0, symbolsRead);
+                bufferedWriter.write(buffer, 0, symbolsRead);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return text.toString();
-    }
-
-    public static boolean printTextIntoFileUTF16(String text) {
-
-        try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(OUTPUT_FILE_NAME), "UTF-16")) {
-            outputStreamWriter.write(text);
-            outputStreamWriter.flush();
+            bufferedWriter.flush();
+            return true;
         } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
-        return true;
     }
 }
